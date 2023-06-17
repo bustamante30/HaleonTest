ae3ba7f197d6f1fd52fb33a44beb09129486c469
Ahila <ahilsnekapriya.balakrishnan@peerislands.io>
2023-06-16 19:27:49 -0500

    Revert "sample iframe changes"
    This reverts commit 4659bdbfd1f7020c37cb9658c9ec235312547b7a.

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -6,11 +6,6 @@ export default [
     component: () => import('@/pages/BasePage.vue'),
   },
   {
-    path: "/water",
-    name: "water",
-    component: () => import('@/pages/WaterFront.vue'),
-  },
-  {
     path: "/b2clogin",
     name: "b2clogin",
     component: () => import('@/pages/auth/B2clogin.vue'),
