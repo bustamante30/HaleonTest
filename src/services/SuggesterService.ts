b05f23a1ae5009203a068c3d28307ec20ed8c04c
Orlando Bustamante - Contractor <orlando.bustamante-CONTRACTOR@sgsco.com>
2023-06-28 18:39:52 -0500

    build fix

--- src/services/SuggesterService.ts
+++ src/services/SuggesterService.ts
@@ -1,5 +1,5 @@
 import { AiCustomerSearchDto } from '../models/AiCustomerSearchDto';
-import ApiService from '../services/ApiService';
+import ApiService from '../services/apiService';
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5208/';
 const httpService = new ApiService(baseUrl)
