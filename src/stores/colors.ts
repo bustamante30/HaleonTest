ed8789b0581293d63957dceeeec03a6852f8d2cd
Ahila <ahilsnekapriya.balakrishnan-contractor@sgsco.com>
2023-07-07 09:42:22 -0500

    Build error fix reorder

--- src/stores/colors.ts
+++ src/stores/colors.ts
@@ -1,13 +1,15 @@
-import colorsData from "@/data/mock/colors";
+// import colorsData from "@/data/mock/colors";
+import type { ColourDto } from "@/models/ColourDto";
 import { defineStore } from "pinia";
+import { useOrdersStore } from "./orders";
 
 export const useColorsStore = defineStore("colorsStore", {
   state: () => ({
-    // colors: [],
-    colors: [] as { colour: string; imageId: string; sets: number }[], // Specify the type of colors
+    colors: [] as ColourDto[], // Specify the type of colors
   }),
   actions: {
     async getColors() {
+      const ordersStore = useOrdersStore()
       this.colors = colorsData;
     }
   },
