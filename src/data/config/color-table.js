81f2a54666e1c0c6e80a7b78526ccac893f945af
Orlando Bustamante - Contractor <orlando.bustamante-CONTRACTOR@sgsco.com>
2023-07-07 22:35:14 -0500

    fixed advanced search lost code, fixed reset button, fixed formatting of thumbnail on advanced search

--- src/data/config/color-table.js
+++ src/data/config/color-table.js
@@ -4,21 +4,22 @@ export default {
         header: 'Client Colour Ref',
         field: 'clientPlateColourRef',
         freeze: 'left',
-        width: 12
+        width: 10
     },
     {
       header: 'SGS Colour',
       field: 'colourName',
       freeze: 'left',
-      width: 12
+      width: 10
     },
     {
       header: 'Image Carrier ID #',
       field: 'imageCarrierId',
       freeze: 'left',
+      width: 12
     },
      {
-       header: 'Number of Sets (Qty)',
+       header: 'Sets (Qty)',
        field: 'sets',
        type: 'number',
        freeze: 'center',
