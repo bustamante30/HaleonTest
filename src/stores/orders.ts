d83c715b95b200f3f8287a2c010455be778a3c5d
Orlando Bustamante - Contractor <orlando.bustamante-CONTRACTOR@sgsco.com>
2023-09-06 11:13:38 -0500

    fix cart orders view

--- src/stores/orders.ts
+++ src/stores/orders.ts
@@ -232,11 +232,11 @@ export const useOrdersStore = defineStore("ordersStore", {
           let details = JSON.parse(
             JSON.stringify(await ReorderService.getOrderDetails(reorderId))
           );
-          if(details.printerName!="")
-            this.selectedOrder.printerName = details.printerName
           const plateTypes = this.mapPlateTypes(details)
           this.options.plateTypeDescription = plateTypes.filter((plateType: any) => plateType.value !== 256)
           this.selectedOrder = this.selectedOrder || {}
+          if(details.printerName!="")
+            this.selectedOrder.printerName = details.printerName
           this.selectedOrder.description = details.jobDescription;
           this.selectedOrder.barcodes = details.barcode;
           this.selectedOrder.packagingReference = details.jobDetails.packagingReference;
