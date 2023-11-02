a1657a0c300da6cda34f067d7b2b412ff6f3be88
Ahila <ahilsnekapriya.balakrishnan-contractor@sgsco.com>
2023-11-01 23:30:36 -0500

    The link in MySGS that directs the PM to the Photon order must direct them to the final confirmation pop-up

---
+++ src/pages/orders/PhotonSuccessPage.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.page.success
+  sgs-mask
+  .container
+    // eslint-disable-next-line vue/attribute-hyphenation
+    photon-success(:selectedId="id")
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import PhotonSuccess from "@/components/orders/PhotonSuccess.vue";
+const route = useRoute();
+const id = route.params.pid;
+</script>
+
+<style lang="sass" scoped>
+@import "@/assets/styles/includes"
+
+@keyframes slide
+  0%
+    transform: translateX(40vw)
+  100%
+    transform: translate(0)
+
+.page.success
+  .container
+    +fixed-e
+    +container
+    width: 85vw
+    min-width: 75rem
+    background: white
+    box-shadow: -10px 0 5px 3px rgba(0, 0, 0, 0.1)
+    z-index: $z-popup-page
+    animation: slide 0.2s ease-in
+</style>
